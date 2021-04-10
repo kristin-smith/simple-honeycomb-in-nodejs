5e9fe89e6fc9958ee8acf84542850c4209876634
Kristin Smith <kristinsmith@Kristins-MacBook-Pro.local>
2021-04-10 15:25:36 -0600

    First event with eventEmitter - logs to logger.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 
-const os = require('os');
-var totalMem = os.totalmem();
-var hostname = os.hostname();
+const EventEmitter = require('events'); //class notation with its own methods
+const emitter = new EventEmitter(); //object, instance of class EventEmitter
+const log = require('./logger')
 
-//Template string
+//Register a listener
+emitter.on('messageLogged', (arg) => {
+  console.log('Listener called', arg)
+});
 
-console.log(`Total Memory: ${totalMem}`);
-console.log(`Hostname: ${hostname}`);
+emitter.on('sendLog', (arg) => {
+    log(arg.data)
+});
+
+// Raise an event
+emitter.emit('messageLogged', {id: 1, url: 'http://'});
+//needs a previously defined listener for anything to happen
+
+// Raise the logging event (data: message)
+emitter.emit('sendLog', { data: 'message'});
---
+++ logger.js
@@ -0,0 +1,7 @@
+
+var url = 'http://mylogger.io/logs'
+function log(message){
+    console.log(`Sending message: ${message}`);
+}
+
+module.exports = log
