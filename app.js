a1619c0e6343d78a63d798a61d87a00963468c3a
Kristin Smith <kristinsmith@Kristins-MacBook-Pro.local>
2021-04-10 18:36:47 -0600

    first pass instrumenting simple nodejs server and responses to http incoming requests

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 
-const EventEmitter = require('events'); //class notation with its own methods
-
-const Logger = require('./logger')
-const logger = new Logger();
-
-//Register a listener
-logger.on('messageLogged', (arg) => {
-  console.log('Listener called', arg)
+require("honeycomb-beeline")({
+    // Get this via https://ui.honeycomb.io/account after signing up for Honeycomb
+    writeKey: "13e407bd57ef0ef0284c958e08cafcad",
+    // The name of your app is a good choice to start with
+    dataset: "first-node-app-dataset",
+    serviceName: "first-node-app"
 });
 
-logger.on('sendLog', (arg) => {
-    console.log('log sent')
+const beeline = require("honeycomb-beeline")();
+console.log(beeline)
+const trace = beeline.startTrace()
+
+const http = require("http");
+const server = http.createServer((req,res) => {
+    if (req.url === '/') {
+        const responseSpan = beeline.startSpan();
+        res.write('Hello Honeycomb!');
+        res.end();
+        beeline.addTraceContext({
+            response: 'Hello honeycomb!'
+        })
+        beeline.finishSpan(responseSpan);
+    }
 });
 
-logger.log('message')
+const serverSpan = beeline.startSpan();
+server.on('connection', (socket) =>{
+    console.log('new connection...')
+})
+beeline.addTraceContext({
+    step: 'starting server'
+})
+server.listen(3000);
+beeline.finishSpan(serverSpan);
+
+console.log('Listening on port 3000');
+beeline.finishTrace(trace);
+
+
+
+
